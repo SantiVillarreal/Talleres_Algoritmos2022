358a72d4033b557f71e3fe7c3a5252e0b94f9bd6
Santiago Villarreal <87994942+SantiVillarreal@users.noreply.github.com>
2022-09-11 18:43:52 -0500

    taller

---
+++ import turtle.py
@@ -0,0 +1,122 @@
+import turtle
+
+t = turtle.Turtle()
+import turtle
+t=turtle.Turtle()
+t.pensize(20)
+t.pencolor("Blue")
+turtle.bgcolor("black")
+t.shape("arrow")
+t.forward(-120)
+t.right(90)
+t.forward(80)
+t.left(90)
+t.forward(120)
+t.right(90)
+t.forward(80)
+t.right(90)
+t.forward(120)
+t.reset()
+
+t = turtle.Turtle()
+import turtle
+t=turtle.Turtle()
+t.pensize(20)
+t.pencolor("Blue")
+t.shape("arrow")
+t.left(115)
+t.forward(150)
+t.right(-130)
+t.forward(150)
+t.reset()
+
+t = turtle.Turtle()
+import turtle
+t=turtle.Turtle()
+t.pensize(20)
+t.pencolor("Blue")
+t.shape("arrow")
+t.right(90)
+t.forward(-130)
+t.left(30)
+t.forward(140)
+t.right(30)
+t.forward(-140)
+t.reset()
+
+
+
+t = turtle.Turtle()
+import turtle
+t=turtle.Turtle()
+t.pensize(20)
+t.pencolor("Blue")
+t.shape("arrow")
+t.forward(140)
+t.right(180)
+t.forward(70)
+t.left(90)
+t.forward(140)
+t.reset()
+
+
+
+t = turtle.Turtle()
+import turtle
+t=turtle.Turtle()
+t.pensize(20)
+t.pencolor("Blue")
+t.shape("arrow")
+t.left(90)
+t.forward(140)
+t.reset()
+
+
+
+
+t = turtle.Turtle()
+import turtle
+t=turtle.Turtle()
+t.pensize(20)
+t.pencolor("Blue")
+t.shape("arrow")
+t.left(115)
+t.forward(150)
+t.right(-130)
+t.forward(150)
+t.reset()
+
+
+
+t = turtle.Turtle()
+import turtle
+t=turtle.Turtle()
+t.pensize(20)
+t.pencolor("Blue")
+t.shape("arrow")
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(170)
+t.right(90)
+t.forward(100)
+t.reset()
+
+
+
+
+t = turtle.Turtle()
+import turtle
+t=turtle.Turtle()
+t.pensize(20)
+t.pencolor("Blue")
+t.shape("arrow")
+t.circle(80)
+t.reset()
